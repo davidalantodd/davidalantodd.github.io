86734c08691e16c44f45ff569dd0ba56de4ce244
davidalantodd <david.todd@multiverse.io>
2024-08-16 22:29:11 -0400

    add chickadee app deployment URL in projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,13 +12,15 @@ function Projects() {
             altText: "musical app screenshot",
             description: "A full-stack app to track musical theatre productions using: C#/.NET, Angular/TypeScript/SCSS, and PostgreSQL",
             githubURL: "https://github.com/davidalantodd/musical-app",
+            deploymentURL: null
         },
         {
             name:"Chickadee",
             screenshot: chickadee,
             altText: "chickadee app screenshot",
-            description: "A front-end app to view recent bird sightings in your area using JavaScript, React, Bootstrap, and eBird API data.",
+            description: "A front-end app to view recent bird sightings in your area using JavaScript, React, Bootstrap, Netlify, and eBird API data.",
             githubURL: "https://github.com/davidalantodd/chickadee",
+            deploymentURL: "https://chickadeeapp.netlify.app"
         },
         {
             name:"Advent of Code",
@@ -26,6 +28,7 @@ function Projects() {
             altText: "advent of code screenshot",
             description: "Solutions to Advent of Code challenge problems, using Ruby, Python, JavaScript, C, and Go",
             githubURL: "https://github.com/davidalantodd/advent-of-code",
+            deploymentURL: null
         }
     ]
     return (
@@ -38,7 +41,10 @@ function Projects() {
                 <article className='project-text'>
                     <p className='project-title'>{p.name}</p>
                     <p className='description'>{p.description}</p>
-                    <p className="sourceCode"> GitHub link: <a href={p.githubURL} target="_blank" rel="noreferrer opener"><img className="github" src={github} alt="Github Logo"/></a></p>
+                    <p className='links'>
+                        <p className="sourceCode"> Source code: <a href={p.githubURL} target="_blank" rel="noreferrer opener"><img className="github" src={github} alt="Github Logo"/></a></p>
+                        {(p.deploymentURL ? <p className="deployedURL"> Deployed app: <a href={p.deploymentURL} target="_blank" rel="noreferrer opener"><span className="material-symbols-outlined github">rocket_launch</span></a></p> : null)}
+                    </p>
                 </article>
             </section>
         })}
